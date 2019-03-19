1c13b2a91c42967f01f917c1bda512301d8e9a1b
Lyla <30636693+Lyla-M@users.noreply.github.com>
2019-03-19 16:20:08 -0400

    Transform Server
    Three tools added

--- UnityIGTLink.cpp
+++ UnityIGTLink.cpp
@@ -36,61 +36,116 @@ extern "C" {
 		}
 	}
 
-
-
 		//------------------------------------------------------------
-	void SendData(igtl::Matrix4x4& matrix, igtl::ServerSocket::Pointer serversocket, igtl::Socket::Pointer clientsocket)
+	void SendProbeToReference(igtl::Matrix4x4& Pmatrix, igtl::ServerSocket::Pointer serversocket, igtl::Socket::Pointer clientsocket)
 	{
-	/*	SendTransform(someMatrix, "ProbeToTracker", someSocket);
-		SendTransform(someOtherMatrix, "ReferenceToTracker", someSocket);*/
+
 		//------------------------------------------------------------
 		
 			// Allocate Transform Message Class
-		    igtl::TrackingDataMessage::Pointer trackingSendMsg = igtl::TrackingDataMessage::New();
-			igtl::TrackingDataElement::Pointer trackingElement0 = igtl::TrackingDataElement::New();
-			trackingElement0->SetName("Tracker2ToReference");
-			trackingElement0->SetType(IGTL_TDATA_TYPE_6D);
-			trackingElement0->SetMatrix(matrix);
-			//------------------------------------------------------------
-			// Allocate TimeStamp class
-			igtl::TimeStamp::Pointer ts;
-			ts = igtl::TimeStamp::New();
+		    igtl::TransformMessage::Pointer PTransformMessage = igtl::TransformMessage::New();
+
 			//------------------------------------------------------------
 			//Package Data then send
-			trackingSendMsg = igtl::TrackingDataMessage::New();
-			trackingSendMsg->SetHeaderVersion(IGTL_HEADER_VERSION_1);
-			trackingSendMsg->SetDeviceName("Tracker8");
-			ts->GetTime();
-			trackingSendMsg->SetTimeStamp(ts);
-			trackingSendMsg->AddTrackingDataElement(trackingElement0);
-
-			trackingSendMsg->Pack();
-			UnitySendSocket->Send(trackingSendMsg->GetPackPointer(), trackingSendMsg->GetPackSize());
-
-			
+			PTransformMessage->SetHeaderVersion(IGTL_HEADER_VERSION_1);
+			PTransformMessage->InitPack();
+			PTransformMessage->SetDeviceName("ProbeToReference");
+			PTransformMessage->SetMatrix(Pmatrix);
+			//ts->GetTime();
+			//TransformMessage->SetTimeStamp(ts);
+			PTransformMessage->Pack();
+			UnitySendSocket->Send(PTransformMessage->GetPackPointer(), PTransformMessage->GetPackSize());			
 			//igtl::Sleep(1); // wait
-		//}
-
+	
 		//------------------------------------------------------------
 		// Close connection
 
 		//socket->CloseSocket();
 		
 	}
+	void SendNeedleToReference(igtl::Matrix4x4& Nmatrix, igtl::ServerSocket::Pointer serversocket, igtl::Socket::Pointer clientsocket)
+	{
+
+		//------------------------------------------------------------
+
+			// Allocate Transform Message Class
+		igtl::TransformMessage::Pointer NTransformMessage = igtl::TransformMessage::New();
+
+		//------------------------------------------------------------
+		//Package Data then send
+		NTransformMessage->SetHeaderVersion(IGTL_HEADER_VERSION_1);
+		NTransformMessage->InitPack();
+		NTransformMessage->SetDeviceName("NeedleToReference");
+		NTransformMessage->SetMatrix(Nmatrix);
+		//ts->GetTime();
+		//TransformMessage->SetTimeStamp(ts);
+		NTransformMessage->Pack();
+		UnitySendSocket->Send(NTransformMessage->GetPackPointer(), NTransformMessage->GetPackSize());
+		//igtl::Sleep(1); // wait
+
+	//------------------------------------------------------------
+	// Close connection
+
+	//socket->CloseSocket();
+
+	}
+	void SendKidneyToReference(igtl::Matrix4x4& Kmatrix, igtl::ServerSocket::Pointer serversocket, igtl::Socket::Pointer clientsocket)
+	{
+
+		//------------------------------------------------------------
+
+			// Allocate Transform Message Class
+		igtl::TransformMessage::Pointer KTransformMessage = igtl::TransformMessage::New();
+
+		//------------------------------------------------------------
+		//Package Data then send
+		KTransformMessage->SetHeaderVersion(IGTL_HEADER_VERSION_1);
+		KTransformMessage->InitPack();
+		KTransformMessage->SetDeviceName("KidneyToReference");
+		KTransformMessage->SetMatrix(Kmatrix);
+		//ts->GetTime();
+		//TransformMessage->SetTimeStamp(ts);
+		KTransformMessage->Pack();
+		UnitySendSocket->Send(KTransformMessage->GetPackPointer(), KTransformMessage->GetPackSize());
+		//igtl::Sleep(1); // wait
+
+	//------------------------------------------------------------
+	// Close connection
+
+	//socket->CloseSocket();
+
+	}
 	UnityIGTLinkAPI void GetData(float *Rorientation, int Osize, float *Rposition, int Psize, void(*CheckPosition)(float ReceivedPosition))
 	{
 
 		// build matrx 
-		igtl::Matrix4x4 matrix;
-		igtl::QuaternionToMatrix(Rorientation, matrix);
-		matrix[0][3] = Rposition[0];
-		matrix[1][3] = Rposition[1];
-		matrix[2][3] = Rposition[2];
+		igtl::Matrix4x4 Pmatrix;
+		float Porientation[] = { Rorientation[0],Rorientation[1],Rorientation[2],Rorientation[3] };
+		igtl::QuaternionToMatrix(Porientation, Pmatrix);
+		Pmatrix[0][3] = Rposition[0];
+		Pmatrix[1][3] = Rposition[1];
+		Pmatrix[2][3] = Rposition[2];
+
+		igtl::Matrix4x4 Nmatrix;
+		float Norientation[] = { Rorientation[4],Rorientation[5],Rorientation[6],Rorientation[7] };
+		igtl::QuaternionToMatrix(Norientation, Nmatrix);
+		Nmatrix[0][3] = Rposition[3];
+		Nmatrix[1][3] = Rposition[4];
+		Nmatrix[2][3] = Rposition[5];
+
+		igtl::Matrix4x4 Kmatrix;
+		float Korientation[] = { Rorientation[8],Rorientation[9],Rorientation[10],Rorientation[11] };
+		igtl::QuaternionToMatrix(Korientation, Kmatrix);
+		Kmatrix[0][3] = Rposition[6];
+		Kmatrix[1][3] = Rposition[7];
+		Kmatrix[2][3] = Rposition[8];
 		
-		float receivedZ = matrix[0][3];
+		float receivedZ = Pmatrix[0][3];
         CheckPosition(receivedZ);
 
-		SendData(matrix,UnityServerSocket, UnitySendSocket);
+		SendProbeToReference(Pmatrix, UnityServerSocket, UnitySendSocket);
+		SendNeedleToReference(Nmatrix, UnityServerSocket, UnitySendSocket);
+		SendKidneyToReference(Kmatrix, UnityServerSocket, UnitySendSocket);
 	}
 }
 	
